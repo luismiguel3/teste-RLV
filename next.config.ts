ec72fdb9811266fd8eb76cc2c46dacba4861aebc
Luis Miguel <luismiguelpirescorrea@gmail.com>
2024-11-22 20:09:08 -0300

    chore: cors fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  headers: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
