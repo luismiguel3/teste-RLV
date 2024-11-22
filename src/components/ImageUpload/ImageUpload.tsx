86f30fdfbd40e9433bf5d21f1cd957b6128ae49f
Luis Miguel <luismiguelpirescorrea@gmail.com>
2024-11-22 20:00:02 -0300

    wip

--- src/components/ImageUpload/ImageUpload.tsx
+++ src/components/ImageUpload/ImageUpload.tsx
@@ -15,41 +15,38 @@ export default function ImageUpload({ image }: { image: File }) {
     }
   }, [image]);
 
-  console.log(process.env.NEXT_PUBLIC_API_URL)
-  console.log(process.env.NEXT_PUBLIC_APP_API_ENDPOINT)
-  console.log(process.env)
+  console.log(process.env.NEXT_PUBLIC_API_URL);
+  console.log(process.env.NEXT_PUBLIC_APP_API_ENDPOINT);
+  console.log(process.env);
 
   async function uploadImage() {
     const formData = new FormData();
     formData.append("file", image);
     try {
-      const apiUrl = process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3000'
-        : process.env.NEXT_PUBLIC_API_URL;
+      const apiUrl =
+        process.env.NODE_ENV === "production"
+          ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+          : "http://localhost:3000";
 
-      const { data } = await axios.post(
-        `${apiUrl}/api/file`,
-        formData,
-        {
-          headers: {
-            "content-type": "multipart/form-data",
-          },
-          onUploadProgress: (progressEvent) => {
-            console.log(
-              `Upload progress: ${Math.round(
-                (progressEvent.loaded / progressEvent.total) * 100
-              )}%`
-            );
-          },
-          onDownloadProgress: (progressEvent) => {
-            console.log(
-              `Download progress: ${Math.round(
-                (progressEvent.loaded / progressEvent.total) * 100
-              )}%`
-            );
-          },
-        }
-      );
+      const { data } = await axios.post(`${apiUrl}/api/file`, formData, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+        onUploadProgress: (progressEvent) => {
+          console.log(
+            `Upload progress: ${Math.round(
+              (progressEvent.loaded / progressEvent.total) * 100
+            )}%`
+          );
+        },
+        onDownloadProgress: (progressEvent) => {
+          console.log(
+            `Download progress: ${Math.round(
+              (progressEvent.loaded / progressEvent.total) * 100
+            )}%`
+          );
+        },
+      });
     } catch (error) {
       console.log(error);
     }
